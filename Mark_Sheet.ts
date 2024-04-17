16c6fc82bcfa00513e11481758c439f7bb2c664d
Hamza Mughal <163909829+hamzamughal8063@users.noreply.github.com>
2024-04-18 02:09:30 +0500

    Add files via upload
    Mark_Sheet.TS

---
+++ Mark_Sheet.ts
@@ -0,0 +1,15 @@
+const urdu = 75;
+const math = 45;
+const english = 62;
+const scinse = 55;
+const grammer = 50;
+const totalMarks =500;
+
+console.log(totalMarks);
+
+const obtainedMarks = urdu + math + english + scinse + grammer;
+
+console.log(obtainedMarks);
+
+const percentage = obtainedMarks/ totalMarks * 100
+console.log(percentage);
